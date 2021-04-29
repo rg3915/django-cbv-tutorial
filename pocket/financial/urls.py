a4ee09ba31e7e5bac6cae6c627aaf16469def05b
Regis Santos <regis42santos@gmail.com>
2021-04-29 18:05:06 -0300

    Comment urls.

--- pocket/financial/urls.py
+++ pocket/financial/urls.py
@@ -5,10 +5,10 @@ from pocket.financial import views as v
 app_name = 'financial'
 
 urlpatterns = [
-    path('', v.ExpenseListView.as_view(), name='expense_list'),
-    path('not-paid/', v.ExpenseNotPaidListView.as_view(), name='expense_not_paid_list'),  # noqa E501
-    path('<int:pk>/', v.ExpenseDetailView.as_view(), name='expense_detail'),
-    path('create/', v.ExpenseCreateView.as_view(), name='expense_create'),
-    path('<int:pk>/update/', v.ExpenseUpdateView.as_view(), name='expense_update'),
-    path('<int:pk>/delete/', v.ExpenseDeleteView.as_view(), name='expense_delete'),
+    # path('', v.ExpenseListView.as_view(), name='expense_list'),
+    # path('not-paid/', v.ExpenseNotPaidListView.as_view(), name='expense_not_paid_list'),  # noqa E501
+    # path('<int:pk>/', v.ExpenseDetailView.as_view(), name='expense_detail'),
+    # path('create/', v.ExpenseCreateView.as_view(), name='expense_create'),
+    # path('<int:pk>/update/', v.ExpenseUpdateView.as_view(), name='expense_update'),
+    # path('<int:pk>/delete/', v.ExpenseDeleteView.as_view(), name='expense_delete'),
 ]
